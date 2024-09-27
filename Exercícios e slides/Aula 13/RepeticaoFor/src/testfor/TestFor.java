07b4fc8b5ea17dfc36a638d30e66a40e64a797c0
Daniel Pereira <danielp6r@gmail.com>
2024-09-27 10:43:27 -0300

    Update Aula 13

---
+++ Exercícios e slides/Aula 13/RepeticaoFor/src/testfor/TestFor.java
@@ -0,0 +1,25 @@
+package testfor;
+
+/**
+ *
+ * @author daniel
+ */
+public class TestFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=5; cc<=15; cc+=5){
+            System.out.println(cc);
+        }
+        for (int cc = 20; cc>10; cc-=2){
+            System.out.println(cc);
+        }
+        for (int cc=0; cc<=100; cc+=10){
+            System.out.println(cc);
+        }
+    }
+    
+}
